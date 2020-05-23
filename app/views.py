e090b94e664265b97b33cd76cea32f8ffdc0740a
unknown <sonalikul1999@gmail.com>
2020-05-24 00:51:28 +0530

    sonali commit2

---
+++ app/views.py
@@ -0,0 +1,75 @@
+from django.shortcuts import render
+
+# Create your views here.
+def about(request):
+	return render(request,'about.html',{})
+def blog(request):
+	return render(request,'blog.html',{})
+def contact(request):
+	return render(request,'contact.html',{})
+def elements(request):
+	return render(request,'elements.html',{})
+def index(request):
+	return render(request,'index.html',{})
+def industries(request):
+	return render(request,'industries.html',{})
+def main(request):
+	return render(request,'main.html',{})
+def singleblog(request):
+	return render(request,'single-blog.html',{})
+def work(request):
+	return render(request,'work.html',{})
+def shopabout(request):
+	return render(request,'shoppages/about.html',{})
+def shopblog(request):
+	return render(request,'shoppages/blog.html',{})
+def shopblogsingle(request):
+	return render(request,'shoppages/blog.html',{})
+def shopcart(request):
+	return render(request,'shoppages/cart.html',{})
+def shopcheckout(request):
+	return render(request,'shoppages/checkout.html',{})
+def shopcontact(request):
+	return render(request,'shoppages/contact.html',{})
+def shopindex(request):
+	return render(request,'shoppages/index.html',{})
+def shopproductsingle(request):
+	return render(request,'shoppages/product-single.html',{})
+def shopshop(request):
+	return render(request,'shoppages/shop.html',{})
+def adminindex(request):
+	return render(request,'adminpages/index.html',{})
+def adminpagelogin(request):
+	return render(request,'adminpages/page-login.html',{})
+def adminpageregister(request):
+	return render(request,'adminpages/page-register.html',{})
+def adminpagesforget(request):
+	return render(request,'adminpages/pages-forget.html',{})
+def admintablesbasic(request):
+	return render(request,'adminpages/tables-basic.html',{})
+def admintablesdata(request):
+	return render(request,'adminpages/tables-data.html',{})
+def adminwidgets(request):
+	return render(request,'adminpages/widgets.html',{})
+def adminformsadvanced(request):
+	return render(request,'adminpages/forms-advanced.html',{})
+def adminformsbasic(request):
+	return render(request,'adminpages/forms-basic.html',{})
+def shoppaneldashboard(request):
+	return render(request,'shoppanel/dashboard.html',{})
+def shoppanelicons(request):
+	return render(request,'shoppanel/icons.html',{})
+def shoppanelmap(request):
+	return render(request,'shoppanel/map.html',{})
+def shoppanelnotifications(request):
+	return render(request,'shoppanel/notifications.html',{})
+def shoppanelnucleoicons(request):
+	return render(request,'shoppanel/nucleo-icons.html',{})
+def shoppaneltables(request):
+	return render(request,'shoppanel/tables.html',{})
+def shoppaneltypography(request):
+	return render(request,'shoppanel/typography.html',{})
+def shoppanelupgrade(request):
+	return render(request,'shoppanel/upgrade.html',{})
+def shoppaneluser(request):
+	return render(request,'shoppanel/user.html',{})
---
+++ bazzaars/urls.py
@@ -0,0 +1,58 @@
+"""bazzaars URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from app.views import *
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('about/',about),
+    path('blog/',blog),
+    path('contact/',contact),
+    path('elements/',elements),
+    path('index/',index),
+    path('industries/',industries),
+    path('main/',main),
+    path('singleblog/',singleblog),
+    path('work/',work),
+    path('shopabout/',shopabout),
+    path('shopblog/',shopblog),
+    path('shopblogsingle/',shopblogsingle),
+    path('shopcart/',shopcart),
+    path('shopcheckout/',shopcheckout),
+    path('shopcontact/',shopcontact),
+    path('shopindex/',shopindex),
+    path('shopproductsingle/',shopproductsingle),
+    path('shopshop/',shopshop),
+    path('adminindex/',adminindex),
+    path('adminpagelogin/',adminpagelogin),
+    path('adminpageregister/',adminpageregister),
+    path('adminpagesforget/',adminpagesforget),
+    path('admintablesbasic/',admintablesbasic),
+    path('admintablesdata/',admintablesdata),
+    path('adminwidgets/',adminwidgets),
+    path('adminformsadvanced/',adminformsadvanced),
+    path('adminformsbasic/',adminformsbasic),
+    path('shoppaneldashboard/',shoppaneldashboard),
+    path('shoppanelicons/',shoppanelicons),
+    path('shoppanelmap/',shoppanelmap),
+    path('shoppanelnotifications/',shoppanelnotifications),
+    path('shoppanelnucleoicons/',shoppanelnucleoicons),
+    path('shoppaneltables/',shoppaneltables),
+    path('shoppaneltypography/',shoppaneltypography),
+    path('shoppanelupgrade/',shoppanelupgrade),
+    path('shoppaneluser/',shoppaneluser),
+]
