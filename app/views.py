b2a5b9ee2b4ad410d26ad37828dfd61304f2b11d
aryankul97 <aryankul97@gmail.com>
2020-05-26 21:59:51 +0530

    first commit

--- app/views.py
+++ app/views.py
@@ -122,8 +122,6 @@ def checklogin(request):
 	if request.method=='POST':
 		email=request.POST.get('email')
 		password=request.POST.get('password')
-		print(email)
-		print(password)
 		if StoreData.objects.filter(Store_Email=email, Store_Password=password).exists():
 			return render(request,'shoppanel/index.html',{})
 		else:
