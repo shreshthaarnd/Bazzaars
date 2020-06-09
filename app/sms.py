b817808de172a03a22ef555dd5d9fcb666d533b4
aryankul97 <aryankul97@gmail.com>
2020-06-09 22:49:51 +0530

    first commit

--- app/sms.py
+++ app/sms.py
@@ -2,7 +2,7 @@ import urllib.request
 import urllib.parse
  
 def sendSMS(apikey, numbers, sender, message):
-    data =  urllib.parse.urlencode({'apikey': apikey, 'numbers': numbers,
+    data =  urllib.parse.urlencode({'apikey': apikey, 'sender':'BAZZRS', 'numbers': numbers,
         'message' : message})
     data = data.encode('utf-8')
     request = urllib.request.Request("https://api.textlocal.in/send/?")
@@ -11,5 +11,5 @@ def sendSMS(apikey, numbers, sender, message):
     return(fr)
  
 resp =  sendSMS('M231ePl4nWk-rhMFOTxzjqJW4g2qGJXmc3b5zQ7AM1', '918938979011',
-    'Bazzaars', 'Hi Sonali Kaam pe aa jao')
+    'Bazzaars', 'Welcome to Bazzaars.com, Your verification code is 01234')
 print (resp)
